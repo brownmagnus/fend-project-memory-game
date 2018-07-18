dc13e87f092d6af53ffd2eee2423a05b2287b89e
Brownmagnus Olivers <brownmagnuso@gmail.com>
2018-07-18 15:41:11 -0400

    Put the restart Button into use

--- js/app.js
+++ js/app.js
@@ -54,3 +54,5 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+var restartGame = document.querySelector('.restart');
+restartGame.addEventListener('click', initGame);
